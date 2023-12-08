2fcb3165e21eea4464e6d77be88ef88603877b3d
yangyuhang6 <147262846+yangyuhang6@users.noreply.github.com>
2023-12-08 09:55:27 +0800

    Feature: puppeteer and eslint-config-egg

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.emulate(puppeteer.devices['iPhone 6']);
+  await page.goto('https://www.nytimes.com/');
+  await page.screenshot({ path: 'full.png', fullPage: true });
+  await browser.close();
+})();
---
+++ puppeteer.config.cjs
@@ -0,0 +1,10 @@
+/* eslint-disable no-restricted-modules */
+const { join } = require('path');
+
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Changes the cache location for Puppeteer.
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
