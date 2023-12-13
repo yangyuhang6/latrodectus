c6e3e1a43fd1ca604a6677bce67501545b7a7e64
yumiao0220 <1379574846@qq.com>
2023-12-13 22:21:23 +0800

    Feature: yt1998.com add browseList.js & info.js & look.js & storeList.js & supplyInfo.js

---
+++ yt1998.com/browseList.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+
+const print = console.log;
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  try {
+    const targetID = '.rank';
+    await page.goto('https://www.yt1998.com/supplyInfo.html');
+    await page.waitForSelector(targetID, {
+      timeout: 5000,
+    });
+
+    await page.exposeFunction('targetID', () => targetID);
+
+    const news = await page.evaluate(async () => {
+      const container = document.querySelector(await targetID());
+
+      if (container === null) {
+        throw new Error(`div${await targetID()} not exists`);
+      }
+
+      return Array.from(container.querySelectorAll('a')).map(anchor => ({
+        text: anchor.innerHTML.trim(),
+      }));
+    });
+
+    print(news, news.length);
+  } catch (e) {
+    print(e);
+  }
+
+  await page.close();
+  await browser.close();
+})();
